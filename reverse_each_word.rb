103c7e5234996aafed6cc3129081b27dab93fcac
Sonya Gould <sonyavgould@gmail.com>
2020-06-14 22:22:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -11,3 +11,10 @@ def reverse_each_word(array)
   reversed_array = new_array.each {|x| x.reverse!}
   return reversed_array.join(" ")
 end
+
+
+def reverse_each_word(array)
+  new_array = array.split(" ")
+  new_array.collect {|x| x.reverse!}
+  new_array.join(" ")
+end
