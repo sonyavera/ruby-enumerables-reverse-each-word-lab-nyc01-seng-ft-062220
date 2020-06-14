94f6bcb748702b71bfc814f7a73f41916cc25275
Sonya Gould <sonyavgould@gmail.com>
2020-06-14 22:19:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(sentence)
   sentence_array = sentence.split(' ')
   sentence_array.each do |word|
-  word.reverse_each
+  new_array << word.reverse
   end
-reversed_array = array.reverse_each
+new_array = array.reverse_each
 reversed_array
 end
