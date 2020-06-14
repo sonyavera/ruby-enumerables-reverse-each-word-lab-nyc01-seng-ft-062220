e2f00627f18d2dd736542d06dd18620ccc7e3fd9
Sonya Gould <sonyavgould@gmail.com>
2020-06-14 22:27:50 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   sentence_array = sentence.split("" "")
-  new array = []
-  sentence_array.each {|x| new_array << x.reverse}
+  new_array = []
+  new_array = sentence_array.each {|x| x.reverse}
 new_array.join(' ')
 end
 
